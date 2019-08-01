60e1a3d813b802b4690c73e5074280f9155a7f2f
MoanaGR <43298612+HardSpirit@users.noreply.github.com>
2019-08-01 17:35:50 +0200

    ajout de correctif
    correctif mineur

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ var http = require('http');
 var url = require('url');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('bodyParser');
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "",
+  password: ""
+});
 
 var app = express();
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname +'/index.html'));
 })
-.get('/envoiFormulaire', function (req, res){
+.post('/post/', function (req, res){
 
 })
 
