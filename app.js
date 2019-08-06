f77575f7d8519d9a14d642e82acfe9a1e741f914
MoanaGR <43298612+HardSpirit@users.noreply.github.com>
2019-08-06 16:21:10 +0200

    0.0.1-dev
    Création des vues admin client et liaison bdd

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-var http = require('http');
-var url = require('url');
-var express = require('express');
-var path = require('path');
-var bodyParser = require('bodyParser');
-var dbase = require('baseDonnee');
+const http = require('http');
+const url = require('url');
+const express = require('express');
+const path = require('path');
+const dbase = require('baseDonnee');
+const bodyParser = require('body-parser');
+const app = express();
 
+const db = new DataBase();
 
-var app = express();
-
-app.get('/', function(req, res){
+app.use(bodyParser.urlencoded({ extended: false }))
+.get('/', function(req, res){
   res.sendFile(path.join(__dirname +'/index.html'));
 })
-.post('/post/', function (req, res){
-
+.post('/post', function (req, res){
+  const postBody = req.body;
+  var nom = postBody.nom ;
+  var service = postBody.service ;
+  var magasin = postBody.magasin;
+  var msg = postBody.texte;
+  //db.NewTopic(nom, magasin, service, msg);
+  console.log(db);
+})
+.get('/client',function(req, res){
+  res.render('viewClient.ejs',{});
+})
+.get('/admin', function(req, res){
+  res.render('viewAdmin.ejs',{});
 })
 
 
